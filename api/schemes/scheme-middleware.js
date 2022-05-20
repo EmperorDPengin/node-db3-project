fda4de2ab05cddf78093ea0606be765a89176376
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-20 04:26:24 -0400

    finished MVP

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,6 @@
+// const db = require('../../data/db-config');
+const Schemes = require('./scheme-model');
+const yup = require('yup');
 /*
   If `scheme_id` does not exist in the database:
 
@@ -8,6 +11,16 @@
 */
 const checkSchemeId = (req, res, next) => {
 
+  Schemes.findById(req.params.scheme_id)
+    .then(schemeById => {
+      if (schemeById) {
+        req.scheme = schemeById;
+        next();
+      } else {
+        next({ status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found` })
+      }
+    })
+    .catch(next)
 }
 
 /*
@@ -18,8 +31,36 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
 
+const schemeValidation = yup.object().shape({
+  scheme_name: yup
+    .string()
+    .trim()
+    .typeError("invalid scheme_name")
+    .required("invalid scheme_name")
+})
+
+const validateScheme = (req, res, next) => {
+  schemeValidation.validate(req.body, {
+    strict:true,
+    stripUnknown: true
+  })
+  .then(validate => {
+    Schemes.find()
+      .then(schemes => {
+        let foundsChemes = schemes.filter(scheme => scheme.scheme_name === validate.scheme_name);
+        if (foundsChemes.length < 1) {
+          req.body = validate;
+          next();
+        } else {
+          next({status: 400, message: 'scheme name is taken'})
+        }
+      })
+      .catch(next);
+  })
+  .catch(err => {
+    next({status: 400, message: err.message});
+  })
 }
 
 /*
@@ -31,7 +72,37 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
+
+const stepValidation = yup.object().shape({
+  instructions: yup
+    .string()
+    .trim()
+    .typeError("invalid step")
+    .required("invalid step"),
+  step_number: yup
+    .number()
+    .typeError('invalid step')
+    .required('step number required')
+    .min(1, 'invalid step')
+})
 const validateStep = (req, res, next) => {
+
+    if (req.scheme.steps >= req.body.step_number) {
+      next({ status: 400, message: 'invalid step'})
+    } 
+
+    stepValidation.validate(req.body,
+      {
+        strict: true,
+        stripUnknown: true
+      })
+      .then(validate => {
+        req.body = validate;
+        next()
+      })
+      .catch(err => {
+        next({ status: 400, message: err.message })
+      })
 
 }
 
